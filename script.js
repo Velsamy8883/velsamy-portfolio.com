9a147fbc57324a68a5010d0f4bd7445858a442c0
Velsamy8883 <k.velsamy8883@gmail.com>
2025-05-19 17:02:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menu=document.querySelector('.menu');
-const menuList=document.querySelector('.navbar');
-
-menu.addEventListener('click',() =>{
-    menuList.classList.toggle('.showMenu')
-})
